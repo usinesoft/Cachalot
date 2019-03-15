5e76e55ace07ffecb3d99101f0bc503320937228
dan.ionescu <dan.ionescu-ext@sgcib.com>
2019-03-15 15:15:18 +0100

    publishing script and multi-platform demo clients

---
+++ DemoClients/BookingMarketplaceCore/publish.cmd
@@ -0,0 +1,1 @@
+dotnet publish -c release -o ..\..\bin\Release\DemoClients\Accounts\netcoreapp2.1\
---
+++ publish.cmd
@@ -0,0 +1,13 @@
+pushd CoreHost
+dotnet publish -c release -o ..\bin\Release\DotnetCoreServer\netcoreapp2.1\
+popd
+pushd DemoClients\AccountsCore
+dotnet publish -c release -o ..\..\bin\Release\DemoClients\Accounts\netcoreapp2.1\
+popd
+pushd DemoClients\BookingMarketplaceCore
+dotnet publish -c release -o ..\..\bin\Release\DemoClients\BookingMarketplace\netcoreapp2.1\
+popd
+pushd Cachalot
+nuget pack Cachalot.csproj -IncludeReferencedProjects -Prop Configuration=Release -Symbols
+move *.nupkg ..\bin\Release
+popd

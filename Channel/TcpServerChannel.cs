e78c7bab00143f6317cb015b878b677ab1bbe718
unknown <dan.ionescu@usinesoft.com>
2021-07-25 20:05:07 +0200

    try without nodelay

--- Channel/TcpServerChannel.cs
+++ Channel/TcpServerChannel.cs
@@ -30,7 +30,7 @@ namespace Channel
             _listener = new TcpListener(new IPEndPoint(IPAddress.IPv6Any, port));
             _listener.Server.DualMode = true;
             _listener.Server.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, 0);
-            _listener.Server.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+            //_listener.Server.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
 
             _listener.Start();
 
